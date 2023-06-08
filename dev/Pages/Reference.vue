18aac56012c51cff7b483357cb8338380ec44c70
Mojtaba Khodakhah <mojtaba.khodakhah@karriere.at>
2023-06-08 12:32:35 +0200

    List all inputs in documentation page

--- dev/Pages/Reference.vue
+++ dev/Pages/Reference.vue
@@ -1,5 +1,32 @@
 <template>
-
+    <h2 class="text-3xl mb-4">Native form elements</h2>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div class="mb-4">
+            <p class="mb-2">Passing the following object as input to form will build a browser native input.</p>
+            <p class="mb-2">Browser native form element is the simple HTML element for a browser that can be compiled as form input.</p>
+            <p class="mb-2">Different native element can be defined only with property "<strong>type</strong>". For example: <code class="bg-gray-200 rounded-lg px-2 py-1">type: "email"</code>.</p>
+            <p>Bellow you can find all supported elements.</p>
+        </div>
+        <div class="mb-4"><Code>{{ inputs[0] }}</Code></div>
+    </div>
+    <h3 class="text-2xl mb-4">All supported elements</h3>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div class="mb-4 p-4 border rounded-lg" v-for="(input, key) in inputs">
+            <div class="mb-3">Type: <code class="bg-gray-200 rounded-lg px-2 py-1">{{input.type}}</code></div>
+            <FormInput :form="forms[key]" :input="input"/>
+        </div>
+    </div>
 </template>
 <script setup>
+import inputs from './../examples/inputs.js'
+import LaravelFormSync from "@/LaravelFormSync.vue";
+import FormInput from "@/components/FormInput.vue";
+import Code from "../Components/Code.vue";
+import {useForm} from "@inertiajs/vue3";
+const forms = [], formValues = []
+inputs.forEach(function (item, key) {
+    formValues[key] = {}
+    formValues[key][item.key] = ''
+    forms[key] = useForm(formValues[key])
+})
 </script>
---
+++ dev/examples/inputs.js
@@ -0,0 +1,119 @@
+export default [
+    {
+        type: "text",
+        key: "demo_text",
+        default: "",
+        label: "Text",
+        placeholder: "Placeholder text"
+    },
+    {
+        type: "email",
+        key: "demo_email",
+        default: "",
+        label: "Email",
+        placeholder: "Placeholder text"
+    },
+    {
+        type: "number",
+        key: "demo_number",
+        default: "",
+        label: "Number",
+        placeholder: "Placeholder text"
+    },
+    {
+        type: "url",
+        key: "demo_url",
+        default: "",
+        label: "Url",
+        placeholder: "Placeholder text"
+    },
+    {
+        type: "datetime-local",
+        key: "demo_datetime_local",
+        default: "",
+        label: "Datetime Local",
+        placeholder: "Placeholder text"
+    },
+    {
+        type: "month",
+        key: "demo_month",
+        default: "",
+        label: "Month",
+        placeholder: "Placeholder text"
+    },
+    {
+        type: "week",
+        key: "demo_week",
+        default: "",
+        label: "Week",
+        placeholder: "Placeholder text"
+    },
+    {
+        type: "time",
+        key: "demo_time",
+        default: "",
+        label: "Time",
+        placeholder: "Placeholder text"
+    },
+    {
+        type: "search",
+        key: "demo_search",
+        default: "",
+        label: "Search",
+        placeholder: "Placeholder text"
+    },
+    {
+        type: "tel",
+        key: "demo_tel",
+        default: "",
+        label: "Tel",
+        placeholder: "Placeholder text"
+    },
+    {
+        type: "checkbox",
+        key: "demo_checkbox",
+        default: true,
+        label: "Checkbox"
+    },
+    {
+        type: "radio",
+        key: "demo_radio",
+        default: true,
+        label: "Radio"
+    },
+    {
+        type: "date",
+        key: "demo_date",
+        default: "01-01-2023",
+        label: "Date"
+    },
+    {
+        type: "ndate",
+        key: "demo_ndate",
+        default: "01-01-2023",
+        label: "NDate"
+    },
+    {
+        type: "textarea",
+        key: "demo_textarea",
+        default: "",
+        label: "Textarea",
+        placeholder: "Placeholder text"
+    },
+    {
+        type: "select",
+        key: "demo_select",
+        default: "1",
+        label: "Select",
+        options: [
+            {
+                value: "1",
+                label: "Option 1"
+            },
+            {
+                value: "2",
+                label: "Option 2"
+            }
+        ]
+    }
+]
